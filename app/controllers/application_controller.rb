849988f7ed46119bec9f17439230675f4563a03f
Tom Golebiowski <golebisquik@gmail.com>
2024-07-14 16:32:30 -0400

    Adding index page & installing bootstrap
    forgot to actually run the bootstrap install command
    adding the index page, so users can view their sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+	def index
+		user = User.find(params[:userId])
+
+		@appointments = []
+		if( user.isCoach )
+			@appointments = Appointment.where(coach: user)
+		else
+			@appointments = Appointment.where(client: user)
+		end
+	end
 end
